e50e4361e7534378e12c68e6813e431c8a0a7593
Douglasli <p1308512@ipm.edu.mo>
2016-02-21 18:21:28 +0800

    database sql

---
+++ ISI.sql
@@ -0,0 +1,133 @@
+-- MySQL dump 10.13  Distrib 5.7.11, for osx10.11 (x86_64)
+--
+-- Host: localhost    Database: ISI
+-- ------------------------------------------------------
+-- Server version	5.7.11
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Cart`
+--
+
+DROP TABLE IF EXISTS `Cart`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Cart` (
+  `itemid` varchar(255) NOT NULL,
+  `uid` varchar(255) NOT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  PRIMARY KEY (`itemid`,`uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `ITEM`
+--
+
+DROP TABLE IF EXISTS `ITEM`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ITEM` (
+  `itemid` varchar(255) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `brand` varchar(255) DEFAULT NULL,
+  `price` decimal(2,0) DEFAULT NULL,
+  `thumbnailimage` varchar(255) DEFAULT NULL,
+  `description` text,
+  `detailphoto` varchar(255) DEFAULT NULL,
+  `category` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`itemid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `OrderItem`
+--
+
+DROP TABLE IF EXISTS `OrderItem`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `OrderItem` (
+  `poNum` varchar(255) NOT NULL,
+  `itemid` varchar(255) NOT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  PRIMARY KEY (`poNum`,`itemid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Orders`
+--
+
+DROP TABLE IF EXISTS `Orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Orders` (
+  `poNum` varchar(255) NOT NULL,
+  `purchaseDate` date DEFAULT NULL,
+  `canDate` date DEFAULT NULL,
+  `canType` varchar(255) DEFAULT NULL,
+  `shipDate` date DEFAULT NULL,
+  `shipAddress` varchar(255) DEFAULT NULL,
+  `status` set('pending','hold','shipped','cancelled') DEFAULT NULL,
+  `uid` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`poNum`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Rating`
+--
+
+DROP TABLE IF EXISTS `Rating`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Rating` (
+  `User_id` varchar(255) NOT NULL,
+  `PoNum` varchar(255) DEFAULT NULL,
+  `itemId` varchar(255) DEFAULT NULL,
+  `stars` int(11) DEFAULT NULL,
+  PRIMARY KEY (`User_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `User`
+--
+
+DROP TABLE IF EXISTS `User`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `User` (
+  `User_id` varchar(255) NOT NULL,
+  `Username` varchar(255) DEFAULT NULL,
+  `First_name` varchar(127) DEFAULT NULL,
+  `Last_name` varchar(127) DEFAULT NULL,
+  `email` varchar(254) DEFAULT NULL,
+  `address` varchar(254) DEFAULT NULL,
+  `password` varchar(254) DEFAULT NULL,
+  `Type` set('vender','customer') DEFAULT NULL,
+  PRIMARY KEY (`User_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-02-21 18:20:16
